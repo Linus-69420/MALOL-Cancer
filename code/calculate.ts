b8c9dc8608e6ea59fe35d13daec2b277dcec89cd
Austromo <moritz.gruber.linz@gmail.com>
2023-11-18 18:53:06 +0100

    fix: small fixes

--- code/calculate.ts
+++ code/calculate.ts
@@ -1,45 +1,32 @@
 import {DB} from "./connection";
 import {GenePairSL} from "./model/GenePairSL";
+import {Database} from "sqlite";
 
 (async() => {
-    const inputGene: number = 84930;
-    let coreScore = 10;
-    let essentialScore = 0;
-    const db = await DB.createDBConnection();
+    if (process.argv.length !== 3 || Number.isNaN(Number(process.argv.slice(2)))) {
+        return;
+    }
+
+    const inputGene: number = Number(process.argv.slice(2));
+    let coreScore: number = 10;
+    const db: Database = await DB.createDBConnection();
 
     let essentiality = await db.get('select Essentiality from Gene where Identifier = ?1', inputGene);
 
     if (essentiality !== undefined && essentiality.Essentiality !== null) {
         console.log("This gene is essential! (" + essentiality.Essentiality + ")")
+        if (essentiality.Essentiality === "CSEGs") {
+            coreScore *= 0.3;
+        }
+        else if (essentiality.Essentiality === "CEGs") {
+            coreScore *= 0.05;
+        }
     }
-
-    if (essentiality === null || essentiality.Essentiality === null) {
-        essentialScore = 1;
-    }
-    else if (essentiality.Essentiality === "CSEGs") {
-        essentialScore = 0.3;
-    }
-    else if (essentiality.Essentiality === "CEGs") {
-        essentialScore = 0.05;
-    }
-    coreScore *= essentialScore;
 
     //console.log(coreScore);
 
-    let genePairs: GenePairSL[] = await db.all<GenePairSL[]>(
-        'select * from GenePairSL where Gene1Id = ?1 or Gene2Id = ?2', [inputGene, inputGene]);
-    //console.log(genePairs);
-
     console.log("\nSL Human:");
-    for (const genePair of genePairs) {
-        let score = Math.round((coreScore * (genePair.rStatisticScore / 100)) * 100) / 100;
-        if (genePair.Gene1Id === inputGene) {
-            console.log("InputGene " + inputGene + " + Gene " + genePair.Gene2Id + " = " + score + " (score)");
-        }
-        else {
-            console.log("InputGene " + inputGene + " + Gene " + genePair.Gene1Id + " = " + score + " (score)");
-        }
-    }
+    await getAndLogGenePairs(db, inputGene, coreScore);
 
     let mappingDataMouse = await db.all(
         'select m.Gene2Id from MappingData m join Gene g on (m.Gene2Id = g.Identifier) where m.Gene1Id = ?1 and g.SpeciesId = 3', inputGene);
@@ -52,18 +39,7 @@ import {GenePairSL} from "./model/GenePairSL";
 
     console.log("\nSL Mouse:");
     for (const mdm of mappingDataMouse) {
-        let genePairs: GenePairSL[] = await db.all<GenePairSL[]>(
-            'select * from GenePairSL where Gene1Id = ?1 or Gene2Id = ?2', [mdm.Gene2Id, mdm.Gene2Id]);
-
-        for (const genePair of genePairs) {
-            let score = Math.round((coreScore * (genePair.rStatisticScore / 100)) * 100) / 100;
-            if (genePair.Gene1Id === mdm.Gene2Id) {
-                console.log("Gene " + mdm.Gene2Id + " + Gene " + genePair.Gene2Id + " = " + score + " (score)");
-            }
-            else {
-                console.log("Gene " + mdm.Gene2Id + " + Gene " + genePair.Gene1Id + " = " + score + " (score)");
-            }
-        }
+        await getAndLogGenePairs(db, mdm.Gene2Id, coreScore);
     }
 
     let mappingDataFly = await db.all(
@@ -77,18 +53,23 @@ import {GenePairSL} from "./model/GenePairSL";
 
     console.log("\nSL Fly:");
     for (const mdf of mappingDataFly) {
-        let genePairs: GenePairSL[] = await db.all<GenePairSL[]>(
-            'select * from GenePairSL where Gene1Id = ?1 or Gene2Id = ?2', [mdf.Gene2Id, mdf.Gene2Id]);
-
-        for (const genePair of genePairs) {
-            let score = Math.round((coreScore * (genePair.rStatisticScore / 100)) * 100) / 100;
-            if (genePair.Gene1Id === mdf.Gene2Id) {
-                console.log("Gene " + mdf.Gene2Id + " + Gene " + genePair.Gene2Id + " = " + score + " (score)");
-            }
-            else {
-                console.log("Gene " + mdf.Gene2Id + " + Gene " + genePair.Gene1Id + " = " + score + " (score)");
-            }
-        }
+        await getAndLogGenePairs(db, mdf.Gene2Id, coreScore);
     }
 
 })();
+
+async function getAndLogGenePairs(db: Database, geneId: number, coreScore: number): Promise<void> {
+    let genePairs: GenePairSL[] = await db.all<GenePairSL[]>(
+        'select * from GenePairSL where Gene1Id = ?1 or Gene2Id = ?2', [geneId, geneId]);
+    genePairs.sort((a, b) => b.rStatisticScore - a.rStatisticScore);
+
+    for (const genePair of genePairs) {
+        let score = Math.round((coreScore * (genePair.rStatisticScore / 100)) * 100) / 100;
+        if (genePair.Gene1Id === geneId) {
+            console.log("Gene " + geneId + " + Gene " + genePair.Gene2Id + " = " + score + " (score)");
+        }
+        else {
+            console.log("Gene " + geneId + " + Gene " + genePair.Gene1Id + " = " + score + " (score)");
+        }
+    }
+}
