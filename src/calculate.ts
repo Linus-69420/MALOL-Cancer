4061f0dd85e41795b9901cf34e7c16ff0c478710
Austromo <moritz.gruber.linz@gmail.com>
2023-11-20 20:56:11 +0100

    chore: last cleanup

--- src/calculate.ts
+++ src/calculate.ts
@@ -17,7 +17,9 @@ import * as rl from 'readline-sync';
     let essentiality = await db.get('select Essentiality from Gene where Identifier = ?1', inputGene);
 
     if (essentiality !== undefined && essentiality.Essentiality !== null) {
-        console.log("This gene is essential! (" + essentiality.Essentiality + ")")
+        console.log("==============================================================");
+        console.log("| This gene is essential! (" + essentiality.Essentiality + ") |");
+        console.log("===============================================================");
         if (essentiality.Essentiality === "CSEGs") {
             coreScore *= 0.3;
         }
@@ -28,66 +30,101 @@ import * as rl from 'readline-sync';
 
     //console.log(coreScore);
 
-    console.log("\nSL Human:");
+    console.log("\n=============");
+    console.log("| SL Human: |");
+    console.log("=============");
+
     await getAndLogGenePairs(db, inputGene, coreScore);
 
-    const mappingStmt: Statement = await db.prepare(
+    const humanMappingStmt: Statement = await db.prepare(
         'select m.Gene2Id from MappingData m join Gene g on (m.Gene2Id = g.Identifier) where m.Gene1Id = ?1 and g.SpeciesId = ?2');
+    const animalMappingStmt: Statement = await db.prepare(
+        'select m.Gene1Id from MappingData m join Gene g on (m.Gene1Id = g.Identifier) where m.Gene2Id = ?1 and g.SpeciesId = 1');
 
-    /*
-    let mappingDataMouse = await db.all(
-        'select m.Gene2Id from MappingData m join Gene g on (m.Gene2Id = g.Identifier) where m.Gene1Id = ?1 and g.SpeciesId = 3', inputGene);
-    */
-    await mappingStmt.bind({
+    await humanMappingStmt.bind({
         1: inputGene,
         2: 3
     });
-    const mappingDataHumanToMouse = await mappingStmt.all();
+    const mappingDataHumanToMouse = await humanMappingStmt.all();
 
-    //console.log(mappingDataMouse);
+    console.log("\n==========================");
+    console.log("| Human -> Mouse Mapping |");
+    console.log("==========================");
 
-    console.log("\nHuman -> Mouse Mapping");
     for (const mdhm of mappingDataHumanToMouse) {
         console.log("Human-Gene " + inputGene + " can be mapped to Mouse-Gene " + mdhm.Gene2Id + ".")
     }
 
-    console.log("\nSL Mouse:");
+    console.log("\n=============");
+    console.log("| SL Mouse: |");
+    console.log("=============");
+
+    let allMousePairIds: number[] = [];
     for (const mdhm of mappingDataHumanToMouse) {
-        await getAndLogGenePairs(db, mdhm.Gene2Id, coreScore);
+        allMousePairIds.push(... await getAndLogGenePairs(db, mdhm.Gene2Id, coreScore));
     }
 
-    /*
-    let mappingDataFly = await db.all(
-        'select m.Gene2Id from MappingData m join Gene g on (m.Gene2Id = g.Identifier) where m.Gene1Id = ?1 and g.SpeciesId = 2', inputGene);
-    */
-    await mappingStmt.reset();
-    await mappingStmt.bind({
+    console.log("\n==========================");
+    console.log("| Mouse -> Human Mapping |");
+    console.log("==========================");
+
+    for (const id of allMousePairIds) {
+        await animalMappingStmt.reset();
+        await animalMappingStmt.bind(id);
+        const mappingDataMouseToHuman = await animalMappingStmt.all();
+
+        for (const mdmh of mappingDataMouseToHuman) {
+            console.log("Mouse-Gene " + id + " can be mapped to Human-Gene " + mdmh.Gene1Id + ".")
+        }
+    }
+
+    await humanMappingStmt.reset();
+    await humanMappingStmt.bind({
         1: inputGene,
         2: 2
     });
-    const mappingDataHumanToFly = await mappingStmt.all();
+    const mappingDataHumanToFly = await humanMappingStmt.all();
 
-    //console.log(mappingDataFly);
+    console.log("\n==========================");
+    console.log("| Human -> Fly Mapping |");
+    console.log("==========================");
 
-    console.log("\nHuman -> Fly Mapping");
     for (const mdhf of mappingDataHumanToFly) {
         console.log("Human-Gene " + inputGene + " can be mapped to Fly-Gene " + mdhf.Gene2Id + ".")
     }
 
-    console.log("\nSL Fly:");
+    console.log("\n=============")
+    console.log("| SL Fly: |");
+    console.log("=============")
+
+    let allFlyPairIds: number[] = [];
     for (const mdhf of mappingDataHumanToFly) {
-        await getAndLogGenePairs(db, mdhf.Gene2Id, coreScore);
+        allFlyPairIds.push(... await getAndLogGenePairs(db, mdhf.Gene2Id, coreScore));
     }
 
+    console.log("\n==========================");
+    console.log("| Fly -> Human Mapping |");
+    console.log("==========================");
+
+    for (const id of allFlyPairIds) {
+        await animalMappingStmt.reset();
+        await animalMappingStmt.bind(id);
+        const mappingDataFlyToHuman = await animalMappingStmt.all();
+
+        for (const mdfh of mappingDataFlyToHuman) {
+            console.log("Mouse-Gene " + id + " can be mapped to Human-Gene " + mdfh.Gene1Id + ".")
+        }
+    }
 })();
 
-async function getAndLogGenePairs(db: Database, geneId: number, coreScore: number): Promise<void> {
+async function getAndLogGenePairs(db: Database, geneId: number, coreScore: number): Promise<number[]> {
     let essentialityStmt: Statement = await db.prepare('select Essentiality from Gene where Identifier = ?1');
 
     let genePairs: GenePairSL[] = await db.all<GenePairSL[]>(
         'select * from GenePairSL where Gene1Id = ?1 or Gene2Id = ?2', [geneId, geneId]);
 
     let results: string[][] = [];
+    let everyPairId: number[] = [];
 
     for (const genePair of genePairs) {
         let score: number = Math.round((coreScore * (genePair.rStatisticScore / 100)) * 100) / 100;
@@ -102,6 +139,7 @@ async function getAndLogGenePairs(db: Database, geneId: number, coreScore: numbe
                 score = Math.round(score * 100) / 100;
             }
 
+            everyPairId.push(genePair.Gene2Id);
             results.push([geneId.toString(), genePair.Gene2Id.toString(), score.toString()]);
         }
         else {
@@ -114,15 +152,18 @@ async function getAndLogGenePairs(db: Database, geneId: number, coreScore: numbe
                 score = Math.round(score * 100) / 100;
             }
 
+            everyPairId.push(genePair.Gene1Id);
             results.push([geneId.toString(), genePair.Gene1Id.toString(), score.toString()]);
         }
     }
+
+    results.sort((a, b) => Number(b.at(2)) - Number(a.at(2)));
     logGenePairs(results);
+
+    return everyPairId;
 }
 
 function logGenePairs(results: string[][]): void {
-    results.sort((a, b) => Number(b.at(2)) - Number(a.at(2)));
-
     results.forEach((value: string[], index: number) => {
         if (index === 3) console.log();
         console.log("| Gene " + value.at(0) + " + Gene " + value.at(1) + " = " + value.at(2) + " (score) |");
