780210f4713782397267ca7d2607ec6b1c05f687
LukaCvijic <lukacvijic@outlook.com>
2023-11-20 10:31:59 +0100

    fix: ignoring false ids

--- code/insertMappingData.ts
+++ code/insertMappingData.ts
@@ -3,17 +3,23 @@ import {DB} from "./connection";
 import {Statement} from "sqlite";
 
 (async() => {
+    /*
     let regexHuman: RegExp = /HUMAN/gi;
     let regexFly: RegExp = /DROME/gi;
     let regexMouse: RegExp = /MOUSE/gi;
+    */
 
     let flyMappingData: string = fs.readFileSync('../data/mapping/human-fly-gene-mapping.csv', 'utf-8');
+    /*
     flyMappingData = flyMappingData.replace(regexHuman, "");
     flyMappingData = flyMappingData.replace(regexFly, "");
+    */
 
     let mouseMappingData: string = fs.readFileSync('../data/mapping/human-mouse-gene-mapping.csv', 'utf-8');
+    /*
     mouseMappingData = mouseMappingData.replace(regexHuman, "");
     mouseMappingData = mouseMappingData.replace(regexMouse, "");
+    */
 
     await insertData(flyMappingData, 2);
     await insertData(mouseMappingData, 3);
@@ -21,12 +27,13 @@ import {Statement} from "sqlite";
 
 async function insertData(data: string, speciesId: number): Promise<void> {
     const db = await DB.createDBConnection();
+
     const stmt: Statement = await db.prepare('select count(*) as cnt from Gene where Identifier = ?1');
     const insertGeneStmt: Statement = await db.prepare(`insert into Gene values (?1, ?2, null, ?3)`);
     const insertMappingDataStmt: Statement = await db.prepare(`insert into MappingData values (?1, ?2)`);
 
     for(let line of data.split('\n')) {
-        console.log("[LINE] " + line)
+        //console.log("[LINE] " + line)
         let infos: string[] = line.split('\t');
 
         try {
@@ -38,44 +45,50 @@ async function insertData(data: string, speciesId: number): Promise<void> {
                 let gene1Id: number = Number(infos.at(0));
                 let gene2Id: number = Number(infos.at(1));
 
-                await stmt.reset();
-                await stmt.bind(gene1Id);
-                let result = await stmt.get();
+                if (Number.isNaN(gene1Id) || Number.isNaN(gene2Id)) {
+                    console.log("[INVALID ID] Ignoring this line: " + line)
+                }
 
-                if (result.cnt === 0) {
-                    console.log('insert Gene1');
+                else {
+                    await stmt.reset();
+                    await stmt.bind(gene1Id);
+                    let result = await stmt.get();
 
-                    await insertGeneStmt.reset();
-                    await insertGeneStmt.bind({
+                    if (result.cnt === 0) {
+                        //console.log('insert Gene1');
+
+                        await insertGeneStmt.reset();
+                        await insertGeneStmt.bind({
+                            1: gene1Id,
+                            2: "",
+                            3: 1
+                        });
+                        await insertGeneStmt.run();
+                    }
+
+                    await stmt.reset();
+                    await stmt.bind(gene2Id);
+                    result = await stmt.get();
+
+                    if (result.cnt === 0) {
+                        //console.log('insert Gene2');
+
+                        await insertGeneStmt.reset();
+                        await insertGeneStmt.bind({
+                            1: gene2Id,
+                            2: "",
+                            3: speciesId
+                        });
+                        await insertGeneStmt.run();
+                    }
+
+                    await insertMappingDataStmt.reset();
+                    await insertMappingDataStmt.bind({
                         1: gene1Id,
-                        2: "",
-                        3: 1
+                        2: gene2Id
                     });
-                    await insertGeneStmt.run();
+                    await insertMappingDataStmt.run();
                 }
-
-                await stmt.reset();
-                await stmt.bind(gene2Id);
-                result = await stmt.get();
-
-                if (result.cnt === 0) {
-                    console.log('insert Gene2');
-
-                    await insertGeneStmt.reset();
-                    await insertGeneStmt.bind({
-                        1: gene2Id,
-                        2: "",
-                        3: speciesId
-                    });
-                    await insertGeneStmt.run();
-                }
-
-                await insertMappingDataStmt.reset();
-                await insertMappingDataStmt.bind({
-                    1: gene1Id,
-                    2: gene2Id
-                });
-                await insertMappingDataStmt.run();
             }
         }
 
